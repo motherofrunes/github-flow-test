0199a073bd45f6c2a2707f1829602af7dd02883d
ochimusha <blacktearsdontfall@gmail.com>
2018-08-17 07:52:16 +0900

    完了したTODOの色を変更するように修正しました

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ $("#add").click(function () {
 $(document).on("change", "input[type=checkbox]", function () {
     if($(this).is(":checked")) {
         $(this).parent().css("text-decoration", "line-through");
+        $(this).parent().css("color", "#ccc");
     }
     else{
         $(this).parent().css("text-decoration", "none");
